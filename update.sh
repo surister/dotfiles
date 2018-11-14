ed205bb766b52a7879885500d6525d7226bf5c4c
surister <surister98@gmail.com>
2018-11-14 18:25:24 +0100

    Update I3, compton and polybar

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-dest=$HOME/dotfiles
-echo $dest
+dest=$HOME/dotfiles # Destination path
+dotc=$HOME/.config  # Source path, in this case, /.config
+
+#compton
+cp $dotc/compton.conf $dest
+
+#i3
+cp -r $dotc/i3 $dest
+
+#polybar
+cp -r $dotc/polybar $dest
+  
