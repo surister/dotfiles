9ba0c02438c21d5376d4a0697409d607da9a97f5
surister <surister98@gmail.com>
2019-12-29 18:46:24 +0100

    typo

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -2,4 +2,4 @@
 
 id=$(xinput | grep "Cestus 310 Gaming Mouse" | sed -n 1p | awk '{print $7'} | cut -c 4-)
 
-xinput --set-prop $id 'libinput ACcel Speed' -0.8
+xinput --set-prop $id 'libinput Accel Speed' -0.8
