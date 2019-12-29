1b08c074b97121206a8e3262d4b29f4a5d0943f2
surister <surister98@gmail.com>
2019-12-29 18:44:37 +0100

    Update

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ dest=$HOME/dotfiles # Destination path
 dotc=$HOME/.config  # Source path, in this case, /.config
 
 #compton
-cp $dotc/compton.conf $dest
-cp -r $dotc/custom-compton $dest
+#cp $dotc/compton.conf $dest
+#cp -r $dotc/custom-compton $dest
 
 #i3
 cp -r $dotc/i3 $dest
 
 #polybar
-cp -r $dotc/polybar $dest
+#cp -r $dotc/polybar $dest
 
 # scripts
 cp -r $HOME/scripts $dest 
