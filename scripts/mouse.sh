5e1feb54b707e82b2181ce8832d9f4d5464f79bc
surister <surister98@gmail.com>
2020-03-08 19:26:53 +0100

    update

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
+# Minus is slower
 
 id=$(xinput | grep "Cestus 310 Gaming Mouse" | sed -n 1p | awk '{print $7'} | cut -c 4-)
 
-xinput --set-prop $id 'libinput Accel Speed' -0.8
+xinput --set-prop $id 'libinput Accel Speed' -0.80
